09a63f752f5590aa22ac6473d99dce1b0ab3f8d3
Kevin Mcguigan <kevin.mcguigan@nscc.ca>
2024-07-12 18:31:27 -0300

    added DPQPE

--- scripts/gif_downloader.py
+++ scripts/gif_downloader.py
@@ -14,15 +14,34 @@ def download_gif(url, save_path):
     except Exception as err:
         print(f"Other error occurred: {err}")
 
-def scrub_radar_gifs(product, format, radar_station, start_date, end_date, scale = '1.5', type = 'RAIN'):
+
+
+
+def scrub_radar_gifs(product, format, radar_station, start_date, end_date, type = 'RAIN'):
+    # ie: https://dd.weather.gc.ca/radar/CAPPI/GIF/CASMB/202407102006_CASMB_CAPPI_1.5_RAIN.gif
+    # ie:https://dd.weather.gc.ca/radar/DPQPE/GIF/CASMB/20240710T2006Z_MSC_Radar-DPQPE_CASMB_Rain.gif
     base_url = f"https://dd.weather.gc.ca/radar/{product}/{format}/{radar_station}"
     current_date = start_date
-
     
+    if product == "CAPPI":
+        tString,zString = '',''
+        # ie _CASMB_CAPPI_1.5_RAIN
+        if type == 'RAIN':
+            product_string = f"_{radar_station}_CAPPI_1.5_RAIN"
+        elif type == 'SNOW':
+            product_string = f"_{radar_station}_CAPPI_1.0_SNOW"
+    elif product == "DPQPE":
+        # ie: Z_MSC_Radar-DPQPE_CASMB_Rain
+        tString,zString = 'T','Z'
+        if type == 'RAIN':
+            product_string = f"_MSC_Radar-DPQPE_{radar_station}_Rain"
+        elif type == 'SNOW':
+            product_string = f"_MSC_Radar-DPQPE_{radar_station}_Snow"
+
 
     while current_date <= end_date:
-        formatted_date = current_date.strftime("%Y%m%d%H%M")
-        filename = f"{formatted_date}_{radar_station}_{product}_{scale}_{type}.gif"
+        formatted_date = current_date.strftime(f"%Y%m%d{tString}%H%M{zString}")
+        filename = f"{formatted_date}{product_string}.gif"
         url = f"{base_url}/{filename}"
         save_dir = os.path.join('data', 'radar',product,format,radar_station)
         save_path = os.path.join(save_dir, filename)
@@ -45,6 +64,10 @@ if __name__ == "__main__":
     radar_stations = ["CASGO", "CASMB", "CASCM"] # Halifax, Sydney, Fredericton
     # radar_stations = ["CASGO"] # Halifax, Sydney, Fredericton
 
-    for radar_station in radar_stations:
-        scrub_radar_gifs(product, format, radar_station, start_date, end_date, scale='1.5', type = 'RAIN')
-        scrub_radar_gifs(product, format, radar_station, start_date, end_date, scale='1.0', type = 'SNOW')
+    products = ["CAPPI", "DPQPE"] # both product types
+    # products = ["DPQPE"]
+
+    for product in products:
+        for radar_station in radar_stations:
+            scrub_radar_gifs(product, format, radar_station, start_date, end_date, type = 'RAIN')
+            scrub_radar_gifs(product, format, radar_station, start_date, end_date, type = 'SNOW')
